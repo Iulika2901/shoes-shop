020e32cf3ed9cc334b50957ac5312beee550aeea
Iulika2901 <84437252+Iulika2901@users.noreply.github.com>
2024-07-25 17:42:02 +0300

    Update database.py

--- shoe-site/database.py
+++ shoe-site/database.py
@@ -1,89 +1,144 @@
-
 import sqlite3
 
-#conn = sqlite3.connect(':memory:')
 
 conn = sqlite3.connect('data.db')
+c = conn.cursor()
 
-
-c =conn.cursor() #cursor create
-
-
-many_customers1 = [('6','John','Hann','123_cat@gmail.com','123',17),
-                  ('7','Karl','Misy','KarlM@gmail.com','sss',29),
-                  ('8','Cumb','Amy','Ammmy@gmail.com','shoes',45)
-                 ]
-
-
-
-  
-#functions
-
-      #create database
 def create_db():
-        c.execute(""" CREATE TABLE IF NOT EXISTS customers1(
-          id text,
-          first_name text,
-          last_name  text,
-          email text,
-          password text,
-           age number          
-          )""")
-  #customers1
+    c.execute("""
+        CREATE TABLE IF NOT EXISTS customers1 (
+            id TEXT PRIMARY KEY,
+            first_name TEXT,
+            last_name TEXT,
+            email TEXT,
+            password TEXT,
+            age INTEGER
+        )
+    """)
 
+    c.execute("""
+        CREATE TABLE IF NOT EXISTS shoes (
+            id TEXT PRIMARY KEY,
+            name TEXT,
+            discount INTEGER,
+            price REAL,
+            available BOOLEAN
+        )
+    """)
+
+    c.execute("""
+        CREATE TABLE IF NOT EXISTS cart (
+            customer_id TEXT,
+            shoe_id TEXT,
+            FOREIGN KEY (customer_id) REFERENCES customers1(id),
+            FOREIGN KEY (shoe_id) REFERENCES shoes(id)
+        )
+    """)
+               
+
+               #users functions
 
 def insert_all():
- c.execute("INSERT INTO customers1 VALUES('1','John','Elder','john_elder@gmail.com','123',13)")
- c.execute("INSERT INTO customers1 VALUES('2','Joan','Elon','cactusr@gmail.com','cactus',34)")
- c.execute("INSERT INTO customers1 VALUES('3','Kira','Mis','cactusr@gmail.com','sss',19)")
- c.execute("INSERT INTO customers1 VALUES('4','Mathew','Pop','pop12@gmail.com','12ld',16)")
- c.execute("INSERT INTO customers1 VALUES('5','Karlos','Anders','karrandr@gmail.com','shoes',25)")
- c.executemany("INSERT INTO customers1 VALUES (?,?,?,?,?,?)", many_customers1)
+    many_customers1 = [
+        ('6', 'John', 'Hann', '123_cat@gmail.com', '123', 17),
+        ('7', 'Karl', 'Misy', 'KarlM@gmail.com', 'sss', 29),
+        ('8', 'Cumb', 'Amy', 'Ammmy@gmail.com', 'shoes', 45)
+    ]
+    c.executemany("INSERT INTO customers1 VALUES (?, ?, ?, ?, ?, ?)", many_customers1)
+
+    many_shoes = [
+        ('1', 'Nike Air Max', 10, 99.99, True),
+        ('2', 'Adidas Ultraboost', 15, 120.50, True),
+        ('3', 'Puma Runner', 5, 80.00, False)
+    ]
+    c.executemany("INSERT INTO shoes VALUES (?, ?, ?, ?, ?)", many_shoes)
 
 
+def insert_customer(customer):
+    c.executemany("INSERT INTO customers1 VALUES (?, ?, ?, ?, ?, ?)", customer)
 
-def insert(customer):
-    c.executemany("INSERT INTO customers1 VALUES (?,?,?,?,?,?)", customer)
+
+def add_to_cart(customer_id, shoe_id):
+    c.execute("INSERT INTO cart VALUES (?, ?)", (customer_id, shoe_id))
 
 
 def printf():
- c.execute("SELECT * FROM customers1")
- print("Users:")
- print("________________________________")
- print(c.fetchall()) #c.fetchmany(3) print last 3
+    c.execute("SELECT * FROM customers1")
+    items = c.fetchall()
+    print("Users:")
+    print("________________________________")
+    for item in items:
+        print(item)
 
- items = c.fetchall()
- print("")
- print("for login:")
- print("email  \t\t   password:")
- for item in items:
-      print(item[2] + "\t\t"  + item[3])
- print("")
 
-def passw(): 
- print("Password not strong enough for:")
- print("________________________________") 
- print("set stronger password:")    
- print(" ")
- c.execute("""UPDATE customers1 SET password = 'iuski379os' WHERE password LIKE '123%' OR password LIKE '%789' """)
+def passw():
+    print("Password not strong enough for:")
+    print("________________________________")
+    print("set stronger password:")
+    print(" ")
+    c.execute("UPDATE customers1 SET password = 'iuski379os' WHERE password LIKE '123%' OR password LIKE '%789'")
 
 def age_over18():
- c.execute("SELECT age, * FROM customers1 ORDER BY age CRESC")
- c.execute("DELETE from customers1 WHERE age LIMIT 17")
+    c.execute("DELETE FROM customers1 WHERE age < 18")
+
+
+def view_cart(customer_id):
+    c.execute("""
+        SELECT shoes.name, shoes.price, shoes.discount, shoes.available
+        FROM cart
+        JOIN shoes ON cart.shoe_id = shoes.id
+        WHERE cart.customer_id = ?
+    """, (customer_id,))
+    items = c.fetchall()
+    print(f"Cart for customer {customer_id}:")
+    print("________________________________")
+    for item in items:
+        print(item)
 
 
 
-#call functions 
+                          #shoes
+
+def discount_special_days():
+   c.execute("select date('now')")
+   current_date=c.fetchone()[0]
+   year, month, day = current_date.split('-')
+   if day == '23' and month == '12':
+     c.execute("UPDATE shoes SET discount=50 where discount IN(0,10,15) AND available=1") 
+   else:
+       c.execute("UPDATE shoes SET discount=5 where discount IN(0,10,15) AND available=1") 
+   if day == '01' and month == '01':
+      c.execute("UPDATE shoes SET discount=50 where discount IN(0,10,15) AND available=1") 
+   if day == '07' and month == '07':
+      c.execute("UPDATE shoes SET discount=50 where discount IN(0,10,15) AND available=1") 
+   c.execute("select time('now')")
+   current_time=c.fetchone()[0]
+   hour, minute = current_time.split(':')
+   if hour == '23':
+      c.execute("UPDATE shoes SET discount=25 where discount IN(0,10,15) AND available=1") 
+
+
+
+
+
+
+
+
+      
+#users 
+
 create_db()
-insert()
+insert_all()
 printf()
 passw()
 age_over18()
+add_to_cart('6', '1')
+add_to_cart('6', '2')
+view_cart('6')
 
 
-#print("Command executed succesfully...")
+
+
+
 conn.commit()
-
-conn.close() #close connection
-
-
+conn.close()
